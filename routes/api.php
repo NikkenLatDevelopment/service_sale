cabfb0aa5efaa3d6f5e2433d12e22d0f2ccc54dc
Maikol04 <mgarciar@nikkenlatam.com>
2024-02-07 11:25:35 -0600

    ruta api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Api_VentasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+Route::post('ventaProd', [Api_VentasController::class, 'ventaProd']);
